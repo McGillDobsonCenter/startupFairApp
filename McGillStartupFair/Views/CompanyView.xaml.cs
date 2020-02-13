034ebe7faefdc0793a7ade65cd3ab1d2116b506e
alvindsp <31789674+alvindsp@users.noreply.github.com>
2020-02-13 13:00:26 -0500

    Add files via upload

--- McGillStartupFair/Views/CompanyView.xaml.cs
+++ McGillStartupFair/Views/CompanyView.xaml.cs
@@ -4,17 +4,29 @@ using McGillStartupFair.Models;
 
 using Xamarin.Forms;
 
-
 namespace McGillStartupFair.Views
 {
     public partial class CompanyView : ContentPage
     {
+        public string CompanyName { get; private set; }
+
+        public string CompanyDescription { get; private set; }
+
         public CompanyView(Company company)
         {
+            CompanyName = company.Name;
+
+            CompanyDescription = company.Description;
 
             InitializeComponent();
 
             var assembly = typeof(CompanyView);
+
+            compLogo.Source = ImageSource.FromResource(company.ImagePath, assembly);
+
+
+
+            BindingContext = this;
 
         }
     }
